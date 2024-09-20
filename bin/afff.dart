ac375589083370aa8b41896b805ed9df838bad2b
Admin <aflah6764@gmail.com>
2024-09-20 11:08:21 +0530

    commit

---
+++ bin/afff.dart
@@ -0,0 +1,6 @@
+void main(){
+  print("aflah");
+  print(21);
+  print("tly");
+  
+}
---
+++ bin/variables.dart
@@ -0,0 +1,13 @@
+void main(){
+  String name="aflah";
+  print("my name is $name ");
+  int age=25;
+  print("my age is $age");
+  double mark=65.5;
+  print("my mark is $mark");
+  int a=50;
+  int b=45;
+  int sum=a+b;
+  print("sum=$sum");
+
+}
